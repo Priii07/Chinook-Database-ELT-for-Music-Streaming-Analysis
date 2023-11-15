3f7ed24fd72fdd076b76cfabc53f77850b51b0c1
StatsnFacts Syracuse <statsnfacts722@gmail.com>
2023-11-15 00:26:20 +0000

    added sentiment to obt_sentiment_analysis and tagged the date dimension correctly in obt_storage

--- chinook_main/models/obt_sentiment_analysis.sql
+++ chinook_main/models/obt_sentiment_analysis.sql
@@ -6,13 +6,18 @@ d_customer as (
 ),
 d_track as (
     SELECT * FROM {{ ref('dim_tracks') }}
+),
+d_customer_track as (
+    SELECT * FROM {{ ref('dim_customer_track') }}
 )
 SELECT 
     c.*,
     t.*,
+    ct.sentiment,
     f.sentiment_count,
     f.like_sentiment,
     f.sentiment_dislike
 FROM f_sentiment_analysis f 
 left join d_customer c on f.customerkey = c.customerkey
 left join d_track t on f.trackkey = t.trackkey
+left join d_customer_track ct on f.trackkey = ct.trackkey
--- chinook_main/models/obt_storage.sql
+++ chinook_main/models/obt_storage.sql
@@ -3,10 +3,14 @@ with f_storage as (
 ),
 d_tracks as (
     SELECT * FROM {{ ref('dim_tracks') }}
+),
+d_date as (
+    SELECT * FROM {{ ref('dim_date') }}
 )
 
 SELECT  
     t.*, 
+    d.*,
     f.invoicedatekey,
     f.quantity, 
     f.totalearnings,
@@ -14,3 +18,4 @@ SELECT
     f.tracksizeinmb
 FROM f_storage f 
 LEFT JOIN d_tracks t on f.trackkey = t.trackkey 
+LEFT JOIN d_date d on f.invoicedatekey = d.datekey
