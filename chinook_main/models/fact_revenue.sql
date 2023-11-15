0fbf5541dd3f2e21bdce4864d7cfd73e507f15e0
StatsnFacts Syracuse <statsnfacts722@gmail.com>
2023-11-15 00:48:44 +0000

    created a one big table obt_revenue.sql added the track dimension in the fact table for revenue analysis

--- chinook_main/models/fact_revenue.sql
+++ chinook_main/models/fact_revenue.sql
@@ -12,11 +12,14 @@ stg_invoiceline as (
 ),
 stg_customers as (
     SELECT * FROM {{ source('chinook', 'Customers') }}
+),
+stg_tracks as (
+    SELECT * FROM {{ source('chinook', 'Track') }}
 )
-
 SELECT
     il.invoiceid, 
     {{ dbt_utils.generate_surrogate_key(['c.customerid'])}} as customerkey,
+    {{ dbt_utils.generate_surrogate_key(['t.trackid'])}} as trackkey,
     replace(to_date(i.invoicedate)::varchar,'-','')::int as invoicedatekey,
     il.quantity,
     il.totalearnings,
@@ -28,3 +31,4 @@ SELECT
 FROM stg_invoice i 
 JOIN stg_invoiceline il on i.invoiceid = il.invoiceid 
 JOIN stg_customers c on i.customerid = c.customerid
+join stg_tracks t on il.trackid = t.trackid
