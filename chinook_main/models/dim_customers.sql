9e3c40db015d75596866b5ddf0af017dfdc87295
StatsnFacts Syracuse <statsnfacts722@gmail.com>
2023-11-24 03:41:53 +0000

    made the necessary transformations of removing the null value in dim_customer.sql file

--- chinook_main/models/dim_customers.sql
+++ chinook_main/models/dim_customers.sql
@@ -6,14 +6,29 @@ SELECT
     customerid, 
     concat(lastname , ', ' , firstname) as customernamelastfirst,
     concat(firstname , ' ' , lastname) as customernamefirstlast,
-    company as customercompany,
+    CASE
+        WHEN company is NULL THEN 'no company'
+        ELSE company
+    END as customercompany,
     address as customeraddress,
     city as customercity,
-    state as customerstate, 
-    postalcode as customerpostalcode,
+    CASE 
+        WHEN state is NULL THEN 'no state'
+        ELSE state
+    END as customerstate, 
+    CASE
+        WHEN postalcode is null THEN 'no postalcode'
+        ELSE postalcode
+    END as customerpostalcode,
     country as customercountry, 
-    phone as customerphone,
-    fax as customerfax,
+    CASE
+        WHEN phone is null THEN 'no phone number'
+        ELSE phone
+    END as customerphone,
+    CASE
+        WHEN fax is null THEN 'no fax'
+        ELSE fax
+    END as customerfax,
     email as customeremail,
     supportrepid as customersupportrepid
 FROM stg_customers 
